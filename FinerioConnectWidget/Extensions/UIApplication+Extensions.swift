3259ad2b8d1d124401e24dfa26693e1c2612384d
YisusFin <davidgh@finerioconnect.com>
2022-01-03 18:30:36 -0600

    Adding missing references and files into the FinerioConnectWidget project

--- FinerioConnectWidget/Extensions/UIApplication+Extensions.swift
+++ FinerioConnectWidget/Extensions/UIApplication+Extensions.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 public extension UIApplication {
     class func fcTopViewController(controller: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
